83fc2aae7ea4762a7b7c7d97dfcf222b4c253292
cristinaluna <cristina.luna@live.com>
2022-09-27 16:45:06 +0200

    Migrate maxon_epos_example package to ros2 Humble. Compiled and not tested

--- maxon_epos_example/node/sample.cpp
+++ maxon_epos_example/node/sample.cpp
@@ -5,21 +5,21 @@
  * @date   2019-06-05 23:51:24
  */
 
-#include <ros/ros.h>
-#include <std_msgs/Float32MultiArray.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/Float32MultiArray.hpp>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "sample");
-    ros::NodeHandle nh;
+    rclcpp::init(argc, argv);
+    rclcpp::Node::SharedPtr nh("sample");
 
-    ros::Publisher pub = nh.advertise<std_msgs::Float32MultiArray>("/maxon_bringup/all_position", 1000);
-    std_msgs::Float32MultiArray msg = std_msgs::Float32MultiArray();
+    rclcpp::Publisher::SharedPtr pub = nh->create_publisher<std_msgs::msg::Float32MultiArray>("/maxon_bringup/all_position", 1000);
+    std_msgs::msg::Float32MultiArray msg = std_msgs::msg::Float32MultiArray();
     msg.data.push_back(300000);
     msg.data.push_back(-200000);
     
-    ros::Rate sleep_rate(50);
-    while (ros::ok()) {
+    rclcpp::Rate sleep_rate(50);
+    while (rclcpp::ok()) {
         pub.publish(msg);
         sleep_rate.sleep();
     }
